9558eca1604a2ad9299c5c23b7a22cdb9183688e
amirldn <amirmaula@outlook.com>
2022-04-17 14:03:34 +0100

    create basic code to push serial messages to arduino

--- python/main.py
+++ python/main.py
@@ -1,9 +1,17 @@
 import serial
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-ser.baudrate = 9600
-print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
-ser.close()
+import time
+arduino = serial.Serial('/dev/cu.usbmodem1301', 9600)  # open serial port
+print('Using', arduino.name)         # check which port was really used
+data = arduino.readline()
+print(str(data))
+arduino.write(b'1 NAME:Hold It Down ARTIST:Digga D MIN:2 SEC:31;')
+# while True:
+#     data = arduino.readline()
+#     if data:
+#         print(str(data))  # strip out the new lines for now
+#         # (better to do .read() in the long run for this reason
 
-# Windows method
-# https://stackoverflow.com/questions/65011660/how-can-i-get-the-title-of-the-currently-playing-media-in-windows-10-with-python
+# # write a string
+# arduino.write(b'1 NAME:Hold It Down ARTIST:Digga D MIN:2 SEC:31')
+# time.sleep(3)
+# arduino.close()
